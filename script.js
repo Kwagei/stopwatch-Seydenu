44b9d3b9d28c35b4928aaaa7b313f39dfaf248ec
Seydenu Sam Seneh <seydenusamseneh@gmail.com>
2023-07-04 20:56:45 +0000

    Update stopwatch.html, style.css, and script.js

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ let seconds = 0;
 let milliseconds = 0;
 let timerDisplay = document.getElementById('timer-display');
 
-// implement the below funtion using the setInteral funtion
 function startTimer() {
-
+    timer = setInterval(updateTimer, 10);
 }
 
-
+function updateTimer() {
+    milliseconds++;
+    if (milliseconds === 100) {
+        milliseconds = 0;
+        seconds++;
+    }
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+    timerDisplay.textContent = formatTime(minutes, seconds, milliseconds);
+}
 
 function formatTime(minutes, seconds, milliseconds) {
     return (
@@ -31,4 +41,28 @@ function resetTimer() {
     seconds = 0;
     milliseconds = 0;
     timerDisplay.textContent = '00:00:00';
+}
+
+// Additional features using callbacks
+
+function setTimer(userMinutes) {
+    minutes = userMinutes;
+    timerDisplay.textContent = formatTime(minutes, seconds, milliseconds);
+}
+
+function stopTimer() {
+    clearInterval(timer);
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+    timerDisplay.textContent = '00:00:00';
+}
+
+function restartTimer() {
+    clearInterval(timer);
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+    timerDisplay.textContent = '00:00:00';
+    startTimer();
 }
